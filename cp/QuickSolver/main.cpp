e7457095c664aafe12f4b6f60aa28ecb806bd30c
李哲 <leezear@live.cn>
2018-03-04 16:34:47 +0800

    2

--- cp/QuickSolver/main.cpp
+++ cp/QuickSolver/main.cpp
@@ -15,8 +15,8 @@ int main() {
 	//s.solve(Heuristic::Var::VRH_DOM_WDEG_MIN, Heuristic::Val::VLH_MIN, TimeLimit);
 	s.solve(Heuristic::Var::VRH_DOM_MIN, Heuristic::Val::VLH_MIN, TimeLimit);
 	cout << "time = " << s.statistics().solve_time << endl;
-	cout << "positive = " << s.statistics().num_positives << endl;
-	cout << "num_revisions = " << s.statistics().num_revisions << endl;
+	cout << "positives = " << s.statistics().num_positives << endl;
+	cout << "revisions = " << s.statistics().num_revisions << endl;
 	delete hm;
 	return 0;
 }
